a40fa2be6889d756280d551a18c7bf99665c19e2
OOOA1 <130595702+OOOA1@users.noreply.github.com>
2023-10-11 00:47:35 +0500

    Add files via upload

---
+++ Лабораторная работа 1/task_3.py
@@ -0,0 +1,10 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+# TODO Разделите участников на две команды
+
+quantityPlayers = len(list_players)
+firstHalf = list_players[0:3]
+print(firstHalf)
+
+secondHalf = list_players[3:6]
+print(secondHalf)
