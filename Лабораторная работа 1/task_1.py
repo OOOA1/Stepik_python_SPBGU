49d5607b77c8304abccf01f92c3b7d366480863c
OOOA1 <130595702+OOOA1@users.noreply.github.com>
2023-10-15 20:38:33 +0500

    Update task_1.py

--- Лабораторная работа 1/task_1.py
+++ Лабораторная работа 1/task_1.py
@@ -2,8 +2,12 @@ numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -
 
 # TODO заменить значение пропущенного элемента средним арифметическим
 
-numbers[4] = 0 #Здесь я присваиваю 4 значению списка numbers то есть None значение 0, что бы не получить ошибку синтаксиса
-averageSum = round(sum(numbers) / len(numbers), 2) #Здесь я считаю среднее значение всех значений массива numbers что бы подставить его на место None
-numbers[4] = averageSum #Здесь я присваиваю четвёртому значению списка numbers среднее значение списка
+change_simbol = 4  # Меняю пропущенное число (None)
 
-print("Измененный список:", numbers) #Вывод ответа
+sum_numbers = sum(numbers[:change_simbol]) + sum(numbers[change_simbol + 1:])
+
+count_numbers = len(numbers)
+
+average_sum = sum_numbers / count_numbers
+numbers[change_simbol] = average_sum
+print("Измененный список:", numbers)  # Вывод ответа
